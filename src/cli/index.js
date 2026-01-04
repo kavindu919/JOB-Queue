09901332af1fdb9bddc3d133c5cfc74ad37765eb
kavindu919 <jayakodyk2@gmail.com>
2026-01-04 22:55:34 +0530

    feat: docker init and redis connection establieshed

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
+import 'dotenv/config'
 
 import {Command} from 'commander'
+import redis from '../redis/connection.js'
 
 const program = new Command()
 
@@ -8,6 +10,12 @@ program.name('job-queue').description("Distributed Job Queue System").version("0
 
 program.command('ping').description("Check if queue system is alive").action(()=>{
     console.log("Queue system alive")
+})
+
+program.command('redis:ping').description("Check Redis connectivity").action(async ()=>{
+    const result = await redis.ping()
+    console.log("Redis response:", result);
+    process.exit(0);
 })
 
 program.parse(process.argv);
